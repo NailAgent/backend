bc2cb67b9879df5193866a59da3f8df6bfe36415
Minseo Nam <minseo031364@gmail.com>
2026-05-21 00:24:57 +0900

    fix: servicesPrice 이중 직렬화 문제 해결

--- src/main/java/com/nailagent/backend/domain/shopinfo/dto/Response/ShopinfoResponse.java
+++ src/main/java/com/nailagent/backend/domain/shopinfo/dto/Response/ShopinfoResponse.java
@@ -1,5 +1,6 @@
 package com.nailagent.backend.domain.shopinfo.dto.Response;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +11,7 @@ public class ShopinfoResponse {
     private String businessHour;
     private Integer closedDays;
     private String bookingFormText;
+    @JsonRawValue
     private String servicesPrice;
     private Integer depositAmount;
     private String accountNumber;
