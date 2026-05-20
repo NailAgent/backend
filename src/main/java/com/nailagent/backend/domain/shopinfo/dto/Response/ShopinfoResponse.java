903796f382418c4ba48e575bc10a178e4bdc4eda
Minseo Nam <minseo031364@gmail.com>
2026-05-21 00:28:06 +0900

    refactor: servicesPrice를 일반 문자열로 변경

--- src/main/java/com/nailagent/backend/domain/shopinfo/dto/Response/ShopinfoResponse.java
+++ src/main/java/com/nailagent/backend/domain/shopinfo/dto/Response/ShopinfoResponse.java
@@ -1,6 +1,5 @@
 package com.nailagent.backend.domain.shopinfo.dto.Response;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,7 +10,6 @@ public class ShopinfoResponse {
     private String businessHour;
     private Integer closedDays;
     private String bookingFormText;
-    @JsonRawValue
     private String servicesPrice;
     private Integer depositAmount;
     private String accountNumber;
