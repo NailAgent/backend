95b6121a9cb18bccdd0d79b8f2b8c479b35a00f4
Minseo Nam <minseo031364@gmail.com>
2026-05-28 00:58:44 +0900

    feat: S3 인증 방식을 IAM 역할에서 IAM 사용자 키로 변경

--- src/main/java/com/nailagent/backend/global/s3/S3Config.java
+++ src/main/java/com/nailagent/backend/global/s3/S3Config.java
@@ -3,7 +3,8 @@ package com.nailagent.backend.global.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -13,11 +14,18 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
