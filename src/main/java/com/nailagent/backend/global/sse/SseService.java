a17f6f8f7155b8ebea029b96b74b7d49b77f68f1
Minseo Nam <minseo031364@gmail.com>
2026-06-12 02:55:13 +0900

    test: 오늘의 예약 브리핑 스케줄러 시간을 테스트 목적으로 새벽 3시로 변경

--- src/main/java/com/nailagent/backend/global/sse/SseService.java
+++ src/main/java/com/nailagent/backend/global/sse/SseService.java
@@ -63,7 +63,7 @@ public class SseService {
         }
     }
 
-    @Scheduled(cron = "0 0 8 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
     public void sendDailyBriefing() {
         List<Reservation> reservations = reservationRepository.findAllByReserveDateAndVisitStatusNot(LocalDate.now(), Reservation.VisitStatus.CANCELLED);
 
